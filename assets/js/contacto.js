50edddf4db4fc6c5524481662171f1e264663c42
Bruno Paoletti <bprogramacion14@gmail.com>
2023-11-15 10:48:43 -0300

    Formulario JS a reparar

--- assets/js/contacto.js
+++ assets/js/contacto.js
@@ -1,13 +1,45 @@
 // Contacto
-const forms = document.querySelectorAll('.needs-validation')
 
-       Array.from(forms).forEach(form => {
-       form.addEventListener('submit', event => {
-       if (!form.checkValidity()) {
-         event.preventDefault()
-         event.stopPropagation()
-       }
+//Validacion de formulario
 
-       form.classList.add('was-validated')
-       }, false)
-       })
+ const forms = document.querySelectorAll('.needs-validation')
+
+        Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+        }, false)
+        })
+
+function enviarFormulario() {
+    
+   var formData = {
+       Nombre: document.getElementById("Nombre").value,
+       Apellido: document.getElementById("Apellido").value,
+       Email: document.getElementById("Email").value,
+       Ciudad: document.getElementById("Ciudad").value,
+       Teléfono: document.getElementById("Telefono").value,
+       Consulta: document.getElementById("Consulta").value,
+   }
+   const sheetDBURL = 'http://sheetdb.io/api/v1/9p6nz0f1m0o2s';
+   
+   fetch(sheetDBURL, {
+    
+    method: 'POST',
+       headers: {
+           'Content-Type': 'application/json',
+       },
+       body: JSON.stringify({
+          data: formData,
+       }),
+   })
+   .then(response => response.json())
+   .then(data => {
+       console.log('Elemento agregado:', data);
+   })
+   .catch(error => console.error('Error al agregar elemento:', error));
+ }
